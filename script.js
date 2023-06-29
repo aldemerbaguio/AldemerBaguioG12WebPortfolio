cbf65ac5452d7e4194f4bd3fee805d02d7c633a5
aldemerbaguio <136919940+aldemerbaguio@users.noreply.github.com>
2023-06-30 06:37:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// portfolio.js
+
+function openSection(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => {
+      section.classList.add('hidden');
+    });
+  
+    // Show the selected section
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.remove('hidden');
+  }
+  
